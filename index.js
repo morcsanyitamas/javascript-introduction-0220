cb7ef8415b20cd79470fc9f6fdf962445c5d50fd
morcsanyitamas <mtomi8@gmail.com>
2023-02-22 14:02:28 +0100

    done

--- index.js
+++ index.js
@@ -2,7 +2,33 @@ console.log('Hello');
 console.log('world');
 
 let number = 10;
-number = 1000;
+number = 80;
 
-let numbers = [21, 45, 78, 23, 56, 32, 32];
+let numbers = [21, 45, 78, 23, 56, 32, 33];
 console.log(numbers);
+
+number = 200;
+
+if(number < 100){
+    console.log('number < 100');
+} else {
+    console.log('number > 100');
+}
+
+// for(let index = 0; index < 10000; index++){
+//     console.log('-');
+    
+// }
+
+// let index = 0;
+// while(index < 100){
+    
+//     console.log('-');
+//     index++;
+// }
+
+for(let index = 0; index < 7; index++){
+    console.log(numbers[index]);
+
+}
+
